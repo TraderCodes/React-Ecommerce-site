5a6824851f629e64f63dd91ef42476eefffa7d72
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-22 11:44:14 -0700

    🚀Added- HomePage Third section/EmailSub

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our Newsletter and get 50% off</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In et
+            impedit rem magnam ea deleniti aperiam quia cum, commodi eligendi
+            cupiditate quam perspiciatis nostrum obcaecati maiores voluptatem
+            quidem dolore officiis.
+          </p>
+
+          <form className="contact-form">
+            <input
+              type="email"
+              className="form-input"
+              placeholder="Enter your email address"
+            />
+            <button type="submit" className="submit">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +92,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
