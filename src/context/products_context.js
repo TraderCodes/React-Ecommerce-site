e9e031576285d58d2ad743f29ed24ee109a88225
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-23 04:57:05 -0700

    ✔ Done Fetching products API 0.1 update

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,10 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -30,17 +34,24 @@ export const ProductsProvider = ({ children }) => {
   };
   // ! Fetch
   const fetchProducts = async (url) => {
-    const response = await axios.get(url);
-    console.log(response);
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
   };
 
   useEffect(() => {
     // pass in url from utils files
     fetchProducts(url);
-  },[]);
+  }, []);
   // ----------------------------------------------------
   return (
-    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
+    <ProductsContext.Provider
+      value={{ ...state, openSidebar, closeSidebar, fetchProducts }}
+    >
       {children}
     </ProductsContext.Provider>
   );
