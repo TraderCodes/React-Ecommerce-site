b064501257f9b88f3d42c871c2beb94bbacc0e71
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-24 22:31:29 -0700

    🚀Add to cart amount section done

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -7,9 +7,34 @@ import AmountButtons from './AmountButtons';
 
 const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
+  const [mainColor, setMainColor] = useState(colors[0]);
+  // set amount to display
+  const [amount, setAmount] = useState(1);
+
+  // Clicking on the button will also check if the product is bigger than the stock amount
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      // the amount won't be more than the stock amount
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+  const decrease = () => {
+      setAmount((oldAmount) => {
+        let tempAmount = oldAmount - 1;
+        if (tempAmount < 1) {
+          tempAmount = 1;
+        }
+        return tempAmount;
+      });
+  };
+
   // adding color option
 
-  const [mainColor, setMainColor] = useState(colors[0]);
   return (
     <Wrapper>
       <div className="colors">
@@ -30,11 +55,20 @@ const AddToCart = ({ product }) => {
               >
                 {/* add check on button */}
                 {mainColor === color ? <FaCheck /> : null}
-
               </button>
             );
           })}
         </div>
+      </div>
+      <div className="btn-container">
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to="/cart" className="btn">
+          add to cart
+        </Link>
       </div>
     </Wrapper>
   );
