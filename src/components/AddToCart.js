8603460e4faa1feb31ab0873b43cd07d76020fed
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-25 22:27:33 -0700

    cart update

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from '../context/cart_context';
 import AmountButtons from './AmountButtons';
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
   // set amount to display
@@ -24,13 +25,13 @@ const AddToCart = ({ product }) => {
     });
   };
   const decrease = () => {
-      setAmount((oldAmount) => {
-        let tempAmount = oldAmount - 1;
-        if (tempAmount < 1) {
-          tempAmount = 1;
-        }
-        return tempAmount;
-      });
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
   };
 
   // adding color option
@@ -66,7 +67,7 @@ const AddToCart = ({ product }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to="/cart" className="btn">
+        <Link to="/cart" className="btn" onClick={() => addToCart()}>
           add to cart
         </Link>
       </div>
