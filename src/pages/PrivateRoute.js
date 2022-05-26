54e6d70b1951f6f363243e5cd75790bc3f7099d1
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-26 08:16:39 -0700

    Add priceroute to checkout

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,8 +3,18 @@ import { Route, Redirect } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 // will remove later
 import { useUserContext } from '../context/user_context';
-
-const PrivateRoute = () => {
-  return <h4>Private Route</h4>;
+// the rest on top is rest operator
+const PrivateRoute = ({ children, ...rest }) => {
+  const { myUser } = useUserContext();
+  // use spread to spread the rest
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        // redirect to homepage if user is false
+        return myUser ? children : <Redirect to="/"></Redirect>;
+      }}
+    ></Route>
+  );
 };
 export default PrivateRoute;
